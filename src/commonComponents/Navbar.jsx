d2803a908bfc410acbe138ec7058943f8c0dd291
Ritendra <ritendra.gour@clickspurr.com>
2023-04-15 21:03:11 +0530

    Added Prompt

--- src/commonComponents/Navbar.jsx
+++ src/commonComponents/Navbar.jsx
@@ -3,35 +3,34 @@ import { Link } from 'react-router-dom'
 import { AiOutlineHome } from "react-icons/ai";
 import { FaUser } from "react-icons/fa";
 
-
 const Navbar = () => {
 
     return (
         <>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light px-3">
-                <div class="container-fluid">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light px-3">
+                <div className="container-fluid">
                     <Link className="navbar-brand  p-2" to="/">React World</Link>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         </ul>
-                        <ul class="navbar-nav mr-auto">
-                            <li class="nav-item active">
-                                <Link to="/" className='btn btn-light rounded-5 '> Home</Link>  
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item active">
+                                <Link to="/" className='btn btn-light rounded-5 '> Home</Link>
                                 {/* <AiOutlineHome /> */}
-                            </li> &nbsp;&nbsp;
-                            <li class="nav-item">
-                                <Link to="/about" className='btn btn-light rounded-5 '>About</Link>
-                            </li>&nbsp;&nbsp;
-                            <li class="nav-item">
-                                <Link to="/quote" className='btn btn-light rounded-5 '>Quote APP</Link>
                             </li>
-                            <li class="nav-item">
-                                <Link to="/newsApp" className='btn btn-light rounded-5 '>News APP</Link>
-                            </li>&nbsp;&nbsp;
-                            <li class="nav-item">
+                            <li className="nav-item">
+                                <Link to="/about" className='btn btn-light rounded-5 '>About</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/quote" className='btn btn-light rounded-5 '>Quote App</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/newsApp" className='btn btn-light rounded-5 '>News App</Link>
+                            </li>
+                            <li className="nav-item">
                                 <Link to="/qr" className='btn btn-light rounded-5 '>URL TO QR</Link>
                             </li>
                         </ul>
--- src/qoute/qouteComponents/Card.jsx
+++ src/qoute/qouteComponents/Card.jsx
@@ -15,14 +15,21 @@ const Card = () => {
             payload: data
         })
 
-        // useEffect(() => {
-        // handleCLick()
-        // })
         if (!quote) {
             return (<Reload />)
         }
     }
 
+    // name
+    const [name , setName] = useState('Waiting')
+    
+    const enterName = ()=>{
+        setName(prompt("Please Enter Your Name"))
+    }
+    useEffect(() => {
+        enterName();
+    },[])
+    
     return (
         <div className="card container col-xl-12 col-sm-12 col-md-8 mt-2 mb-2">
             <div className="card-header h2">
@@ -30,15 +37,17 @@ const Card = () => {
             </div>
             <div className="card-body quote-main">
                 <span>
+                    <h1>Hey, </h1>
+                    <h1 className='text-center pt-2 pb-2'>{name}</h1>
                     <p className="card-text h5">{quote.content}</p>
                     <img src={"https://source.unsplash.com/random/900%C3%97700/?" + quote.tags[0]} alt="IMG" width="100%" height="300px" className='ShowHide' />
-                    <h5 className="card-title">{quote.author}</h5>
+                    <h5 className="card-title mt-3">{quote.author}</h5>
                     <div className="container my-1">
                         <p className="badge bg-secondary">{quote.tags[0]}</p>
                         <p className="badge bg-secondary">{quote.tags[1]}</p>
                         <p className="badge bg-secondary">{quote.tags[2]}</p>
                     </div>
-                    <button className="btn btn-primary" onClick={handleCLick}>NEXT <FaGreaterThan /><FaGreaterThan /><FaGreaterThan /></button>
+                    <button className="btn btn-primary w-50" onClick={handleCLick}>NEXT <FaGreaterThan /><FaGreaterThan /><FaGreaterThan /></button>
                 </span>
                 <img src={"https://source.unsplash.com/random/900%C3%97700/?" + quote.tags[0]} alt="IMG" width="100%" height="100%" className='HideShow' />
             </div>
